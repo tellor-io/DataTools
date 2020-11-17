dda5a1df304683843e3b2730fc4012caef86d1c2
tallywiesenberg <t.j.wies@gmail.com>
2020-11-17 09:33:28 -0500

    transfer event logs pulled

--- PyTellor/src/transfers.py
+++ PyTellor/src/transfers.py
@@ -6,19 +6,19 @@ import requests
 from keys import *
 
 #paramaters for query
-start_block = 9000000
+start_block = 8000000
 last_block = w3.eth.blockNumber
 entries = []
 # transfers = tellor_core.events.Transfer.createFilter(fromBlock=start_block)
 
 # events = transfers.get_all_entries()
-for i in range(last_block - 6000, last_block, 2000):
+for i in range(start_block, last_block, 2000):
 
     transfers = tellor_core.events.Transfer.createFilter(fromBlock=i, toBlock=i + 2000)
-    new_events = transfers.get_new_entries()
+    new_events = transfers.get_all_entries()
     entries.append(new_events)
+    print(len(new_events))
     
-print(entries)
 print(len(entries))
 
 # print(events[:5])
